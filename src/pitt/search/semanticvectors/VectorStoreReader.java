cbffe82f7a32e1b58c2f63c3ef3e1528a3bbfbf5
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2008-02-06 15:37:25 +0000

    Did some slight refactoring to make for more robust backwards compatibility with old indexes, with usage messages if things go wrong.

--- src/pitt/search/semanticvectors/VectorStoreReader.java
+++ src/pitt/search/semanticvectors/VectorStoreReader.java
@@ -1,36 +1,36 @@
 /**
-Copyright (c) 2007, University of Pittsburgh
+   Copyright (c) 2007, University of Pittsburgh
 
-All rights reserved.
+   All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
 
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
+   * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
 
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
+   * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
 
-    * Neither the name of the University of Pittsburgh nor the names
-      of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written
-      permission.
+   * Neither the name of the University of Pittsburgh nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **/
 
 package pitt.search.semanticvectors;
@@ -42,111 +42,120 @@ import java.lang.Float;
 import java.util.StringTokenizer;
 
 /**
-This class provides methods for reading a VectorStore from disk. <p>
+   This class provides methods for reading a VectorStore from disk. <p>
 
-The serialization currently presumes that the object (in the ObjectVectors)
-should be serialized as a String. <p>
+   The serialization currently presumes that the object (in the ObjectVectors)
+   should be serialized as a String. <p>
 
-The implementation uses Lucene's I/O package, which proved much faster
-than the native java.io.DataOutputStream
-@see ObjectVector
-*/
+   The implementation uses Lucene's I/O package, which proved much faster
+   than the native java.io.DataOutputStream
+   @see ObjectVector
+**/
 public class VectorStoreReader implements VectorStore {
-    private IndexInput indexInput;
+  private IndexInput indexInput;
+  private boolean hasHeader;
 
-    public VectorStoreReader (String vectorFile) throws IOException {
-	MMapDirectory dir = new MMapDirectory();
-	indexInput = dir.openInput(vectorFile);
-	try {
-	    /* Read number of dimensions from header information. */
-	    String test = indexInput.readString();
-	    if ((test.equalsIgnoreCase("dimensions"))) {
-		ObjectVector.vecLength = Math.round(Float.intBitsToFloat(indexInput.readInt()));
-		System.out.println("Dimensions = " + ObjectVector.vecLength); 
-	    }
-	    else {
-		System.out.println("No file header for file "+vectorFile);
-		System.out.println("Attempting to process with vector length " + 
-				   ObjectVector.vecLength);
-	    }
-	} catch (IOException e) {
-	    System.out.println("Cannot read file: " + vectorFile + "\n" + e.getMessage());
-	}
+  public VectorStoreReader (String vectorFile) throws IOException {
+    MMapDirectory dir = new MMapDirectory();
+    indexInput = dir.openInput(vectorFile);
+    try {
+      /* Read number of dimensions from header information. */
+      String test = indexInput.readString();
+      /* Include "-" character to avoid unlikely case that first term is "dimensions"! */
+      if ((test.equalsIgnoreCase("-dimensions"))) {
+        ObjectVector.vecLength = indexInput.readInt();
+        System.err.println("Dimensions = " + ObjectVector.vecLength);
+        hasHeader = true;
+      }
+      else {
+        System.err.println("No file header for file " + vectorFile +
+                           "\nAttempting to process with default vector length: " +
+                           ObjectVector.vecLength +
+                           "\nIf this fails, consider rebuilding indexes - existing " +
+                           "ones were probably created with old version of software.");
+        hasHeader = false;
+      }
+    } catch (IOException e) {
+      System.out.println("Cannot read file: " + vectorFile + "\n" + e.getMessage());
+    }
+  }
+
+  public Enumeration getAllVectors(){
+    try{
+      indexInput.seek(0);
+      if (hasHeader) {
+        indexInput.readString();
+        indexInput.readInt();
+      }
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new VectorEnumeration(indexInput);
+  }
+
+  /**
+   * given an object, get its corresponding vector <br>
+   * this implementation only works for string objects so far <br>
+   * @param desiredObject - the string you're searching for
+   */
+  public float[] getVector(Object desiredObject) {
+    System.err.print("Seeking vector for ... " + desiredObject + " ... ");
+    try {
+      indexInput.seek(0);
+      if (hasHeader) {
+        indexInput.readString();
+        indexInput.readInt();
+      }
+      while (indexInput.getFilePointer() < indexInput.length() - 1) {
+        if (indexInput.readString().equals(desiredObject)) {
+          System.err.println("Found it ...");
+          float[] vector = new float[ObjectVector.vecLength];
+          for( int i=0; i<ObjectVector.vecLength; i++ ){
+            vector[i] = Float.intBitsToFloat(indexInput.readInt());
+          }
+          return vector;
+        }
+        else{
+          indexInput.seek(indexInput.getFilePointer() + 4*ObjectVector.vecLength);
+        }
+      }
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.err.println("Didn't find it ...");
+    return null;
+  }
+
+  /**
+   * Implements the hasMoreElements() and nextElement() methods
+   * to give Enumeration interface from store on disk.
+   */
+  public class VectorEnumeration implements Enumeration {
+    IndexInput indexInput;
+
+    public VectorEnumeration(IndexInput indexInput){
+      this.indexInput = indexInput;
     }
 
-    public Enumeration getAllVectors(){
-	try{
-	    indexInput.seek(0);
-		indexInput.readString();
-		indexInput.readInt();
-	}
-	catch( IOException e ){
-	    e.printStackTrace();
-	}
-	return new VectorEnumeration(indexInput);
+    public boolean hasMoreElements() {
+      return (indexInput.getFilePointer() < indexInput.length());
     }
 
-    /**
-     * given an object, get its corresponding vector <br>
-     * this implementation only works for string objects so far <br>
-     * @param desiredObject - the string you're searching for
-     */
-    public float[] getVector(Object desiredObject) {
-	System.err.print("Seeking vector for ... " + desiredObject + " ... ");
-	try {
-	    indexInput.seek(0);
-	    indexInput.readString();
-	    indexInput.readInt();
-	    
-	    while (indexInput.getFilePointer() < indexInput.length() -1 ) {
-		if (indexInput.readString().equals(desiredObject)) {
-		    System.err.println("Found it ...");
-		    float[] vector = new float[ObjectVector.vecLength];
-		    for( int i=0; i<ObjectVector.vecLength; i++ ){
-			vector[i] = Float.intBitsToFloat(indexInput.readInt());
-		    }
-		    return vector;
-		}
-		else{
-		    indexInput.seek(indexInput.getFilePointer() + 4*ObjectVector.vecLength);
-		}
-	    }
-	}
-	catch (IOException e) {
-	    e.printStackTrace();
-	}
-	System.err.println("Didn't find it ...");
-	return null;
+    public ObjectVector nextElement() {
+      String object = null;
+      float[] vector = new float[ObjectVector.vecLength];
+      try {
+        object = indexInput.readString();
+        for (int i = 0; i < ObjectVector.vecLength; ++i) {
+          vector[i] = Float.intBitsToFloat(indexInput.readInt());
+        }
+      }
+      catch( IOException e ){
+        e.printStackTrace();
+      }
+      return new ObjectVector(object, vector);
     }
-
-    /**
-     * implements the hasMoreElements() and nextElement() methods 
-     * to give Enumeration interface from store on disk <br>
-     */
-    public class VectorEnumeration implements Enumeration {
-	IndexInput indexInput;
-
-	public VectorEnumeration(IndexInput indexInput){
-	    this.indexInput = indexInput;
-	}
-
-	public boolean hasMoreElements() {
-	    return( indexInput.getFilePointer() < indexInput.length() );
-	}
-
-	public ObjectVector nextElement() {
-	    String object = null;
-	    float[] vector = new float[ObjectVector.vecLength];
-	    try {
-		object = indexInput.readString();
-		for( int i=0; i<ObjectVector.vecLength; i++ ){
-		    vector[i] = Float.intBitsToFloat(indexInput.readInt());
-		}
-	    }
-	    catch( IOException e ){
-		e.printStackTrace();
-	    }
-	    return new ObjectVector( object, vector );
-	}
-    }
+  }
 }
