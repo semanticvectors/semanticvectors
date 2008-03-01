6f89b6d2c0d78983e5213e34059fbc856f443408
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2008-03-01 15:46:42 +0000

    More documentation twiddles to get package info to show up.

---
+++ trunk/src/pitt/search/lucene/package-info.java
@@ -0,0 +1,11 @@
+/**
+
+Package containing some basic Lucene untilities for creating and
+searching indexes, particularly bilingual indexes.
+
+@author Dominic Widdows, in collaboration with Kathleen Ferraro and
+the University of Pittsburgh.
+
+*/
+
+package pitt.search.lucene;
