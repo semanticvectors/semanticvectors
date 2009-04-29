306601b108035fbb25e51da283d0f75100fb7796
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2009-04-29 15:10:04 +0000

    Adding TestUtils

---
+++ trunk/test/pitt/search/semanticvectors/TestUtils.java
@@ -0,0 +1,34 @@
+package pitt.search.semanticvectors;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TestUtils {
+
+	/**
+	 * Utility for taking a command, executing it as a process, and
+	 * returning a scanner of that processes stdout.
+	 */
+	public static Scanner getCommandOutput(String command) {
+	  try {
+	    Runtime runtime = Runtime.getRuntime();
+	    Process process = runtime.exec(command);
+	    Scanner output = new Scanner(process.getInputStream()).useDelimiter("\\n");
+	    process.waitFor();
+	    return output;
+	  }
+	  catch (IOException e) { e.printStackTrace(); }
+	  catch (InterruptedException e) { e.printStackTrace(); }
+	  return null;
+	}
+
+	/**
+	 * Get a term from a search results line.
+	 */
+	public static String termFromResult(String result) {
+	  String[] parts = result.split(":");
+	  if (parts.length != 2) return null;
+	  return parts[1];
+	}
+
+}
