00eeec98d229bc0ea5b66059ede9899f0093fa79
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2009-06-15 18:38:21 +0000

    Cleaned up some of the new Beagle model implementation due to some movement in the SV codebase, e.g., ObjectVector.vecLength -> Flags.dimension.

--- trunk/src-ext/qut/beagle/BeagleUtils.java
+++ trunk/src-ext/qut/beagle/BeagleUtils.java
@@ -37,7 +37,7 @@ package qut.beagle;
 
 import java.util.ArrayList;
 
-import pitt.search.semanticvectors.ObjectVector;
+import pitt.search.semanticvectors.Flags;
 
 import cern.colt.matrix.tfcomplex.impl.DenseFComplexMatrix1D;
 import cern.colt.matrix.tfloat.impl.DenseFloatMatrix1D;
@@ -48,58 +48,58 @@ import cern.jet.random.tdouble.engine.DoubleMersenneTwister;
 import cern.jet.random.tfloat.engine.FRand;
 import cern.jet.random.tfloat.engine.FloatRandomEngine;
 
-public class BeagleUtils 
+public class BeagleUtils
 {
 	private static BeagleUtils instance = null;
 	private Normal normal;
 	private float mu = 0.0f;
 	private float sigma = 0.0f;
-	
+
 	private int convCount = 0;
-		
+
 	/* For performance reasons a FFT cache of a user defined size is used to store
 	 * computed transforms for re-use. This gives a small performance improvement.
 	 */
 	ObjectCache fftCache;
-	
-	protected BeagleUtils() 	
-	{			
+
+	protected BeagleUtils()
+	{
 		fftCache = new ObjectCache(0);
 		normal = new Normal( mu, sigma, new DoubleMersenneTwister( new java.util.Date() ) );
 	}
-		
-	public static BeagleUtils getInstance() 
+
+	public static BeagleUtils getInstance()
 	{
       if(instance == null) {
          instance = new BeagleUtils();
       }
       return instance;
 	}
-	
+
 	public void initialise()
 	{
 		fftCache.clear();
 	}
-	
+
 	public void setFFTCacheSize( int size )
 	{
 		fftCache.resize(size);
 	}
-	
+
 	public void setNormal( float mu, float sigma )
 	{
 		this.mu = mu;
 		this.sigma = sigma;
-		
+
 		normal.setState( mu, sigma );
 	}
-	
-	public int getNumConvolutions() 
+
+	public int getNumConvolutions()
 	{
 		return convCount;
 	}
-	
-	public void printVector(float[] vector) 
+
+	public void printVector(float[] vector)
 	{
 		System.out.println("\n");
 		for (int i = 0; i < vector.length - 1; i++) {
@@ -107,27 +107,27 @@ public class BeagleUtils
 		}
 		// Print last coordinate followed by newline, not "|".
 		System.out.println(vector[vector.length - 1]);
-	}	
-	
+	}
+
 	public void ensureSize( ArrayList<DenseFloatMatrix1D> list, int size )
 	{
 		int lSize = list.size();
 		if (size <= lSize) return;
-		
-		for (int i=lSize; i<size; i++) list.add( null );			
+
+		for (int i=lSize; i<size; i++) list.add( null );
 	}
-		
+
 	public DenseFloatMatrix1D createZeroVector( int size )
 	{
 		DenseFloatMatrix1D A = new DenseFloatMatrix1D(size);
 		for (int i=0; i<A.size(); i++)
 		{
 			A.setQuick(i, 0.0f );
-		}	
-		
+		}
+
 		return A;
 	}
-	
+
 	public void fillVectorRandom( DenseFloatMatrix1D vec, float mu, float sigma )
 	{
 		this.mu = mu; this.sigma = sigma;
@@ -135,293 +135,293 @@ public class BeagleUtils
 		for (int i=0; i<vec.size(); i++)
 		{
 			vec.setQuick(i, (float)normal.nextDouble() );
-		}		
+		}
 	}
-	
+
 	public DenseFloatMatrix1D generateColtRandomVector()
 	{
-		DenseFloatMatrix1D vec = new DenseFloatMatrix1D( ObjectVector.vecLength);
-		
+		DenseFloatMatrix1D vec = new DenseFloatMatrix1D( Flags.dimension);
+
 		this.mu = mu; this.sigma = sigma;
 		normal.setState( mu, sigma );
 		for (int i=0; i<vec.size(); i++)
 		{
 			vec.setQuick(i, (float)normal.nextDouble() );
-		}	
-		
+		}
+
 		return vec;
 	}
-	
+
 	public float[] generateNormalizedRandomVector()
-	{		
-		float[] vec = new float[ObjectVector.vecLength];
-		
+	{
+		float[] vec = new float[Flags.dimension];
+
 		for (int i=0; i<vec.length; i++)
 		{
 			vec[i] = (float)normal.nextDouble();
-		}	
-		
+		}
+
 		normalize(vec);
-		
+
 		return vec;
 	}
-	
+
 	public boolean normalize( float[] vec )
 	{
 		float norm = 0.0f;
-		for (int i = 0; i < vec.length; ++i) 
+		for (int i = 0; i < vec.length; ++i)
 		{
 		    norm += vec[i]*vec[i];
 		}
-				
+
 		norm = (float)Math.sqrt(norm);
-		
-		if (norm==0.0f) 
+
+		if (norm==0.0f)
 		{
 			//System.out.println("\n##########\nERROR - zero norm\n#########");
 			return false;
 		}
-		
-		for (int i = 0; i < vec.length; ++i) 
+
+		for (int i = 0; i < vec.length; ++i)
 		{
 			vec[i] = vec[i]/norm;
 		}
-		
+
 		return true;
 	}
-		
+
 	public DenseFloatMatrix1D normalize( DenseFloatMatrix1D vec )
 	{
 		float sum = vec.zDotProduct( vec, 0, vec.size() );
 		float length = (float)Math.sqrt(sum);
-		
+
 		//if (length==0.0f) System.out.println("\n##########\nERROR - zero norm\n#########");
-		
+
 		if (length != 0.0f) vec.assign( FloatMult.mult(1.0f/length) );
 		return vec;
 	}
-	
+
 	public void addAssignVectors( DenseFloatMatrix1D vec1, DenseFloatMatrix1D vec2, float scale )
 	{
 		if (scale == 1.0f)
 		{
 			for (int i=0; i<vec1.size(); i++)
 			{
-				vec1.setQuick( i, vec1.getQuick(i) + vec2.getQuick(i) ); 
+				vec1.setQuick( i, vec1.getQuick(i) + vec2.getQuick(i) );
 			}
 		}
 		else if (scale == -1.0f)
 		{
 			for (int i=0; i<vec1.size(); i++)
 			{
-				vec1.setQuick( i, vec1.getQuick(i) - vec2.getQuick(i) ); 
+				vec1.setQuick( i, vec1.getQuick(i) - vec2.getQuick(i) );
 			}
 		}
 		else
 		{
 			for (int i=0; i<vec1.size(); i++)
 			{
-				vec1.setQuick( i, vec1.getQuick(i) + vec2.getQuick(i) * scale ); 
+				vec1.setQuick( i, vec1.getQuick(i) + vec2.getQuick(i) * scale );
 			}
 		}
 	}
-	
+
 	public DenseFloatMatrix1D getRandomNormalizedVector( int dim, float mu, float sigma )
 	{
 		this.mu = mu; this.sigma = sigma;
 		DenseFloatMatrix1D A = new DenseFloatMatrix1D(dim);
 		fillVectorRandom( A, mu, sigma );
 		normalize(A);
-		
+
 		return A;
 	}
-	
+
 	public DenseFloatMatrix1D doConvolve( DenseFloatMatrix1D vec1, DenseFloatMatrix1D vec2, int dim )
 	{
-		DenseFloatMatrix1D vec3 = new DenseFloatMatrix1D(dim); 
+		DenseFloatMatrix1D vec3 = new DenseFloatMatrix1D(dim);
 		int idx1, idx2;
-		
+
 		for (int i=0; i<dim; i++ )
 		{
 			for (int j=0; j<dim; j++ )
 			{
 				idx1 = j%dim;
 				idx2 = (i-j)%dim;
-				
+
 				if (idx2 < 0) idx2 = idx2 + dim;
-		        
-				vec3.setQuick( i, vec3.getQuick(i) + vec1.getQuick(idx1) * vec2.getQuick(idx2) ); 
-			}			
+
+				vec3.setQuick( i, vec3.getQuick(i) + vec1.getQuick(idx1) * vec2.getQuick(idx2) );
+			}
 		}
-		
+
 		return vec3;
 	}
-	
+
 	public DenseFComplexMatrix1D getFFT( String str, DenseFloatMatrix1D vec )
 	{
 		DenseFComplexMatrix1D fft;
-		
+
 		if (fftCache.containsKey(str))
 		{
 			fft = (DenseFComplexMatrix1D)fftCache.getObject(str);
 		}
 		else
-		{			
+		{
 			fft = vec.getFft();
 			fftCache.addObject(str, fft);
 		}
-		
+
 		return fft;
 	}
-	
-	public DenseFloatMatrix1D doConvolveFFT( DenseFloatMatrix1D vec1, DenseFloatMatrix1D vec2, 
+
+	public DenseFloatMatrix1D doConvolveFFT( DenseFloatMatrix1D vec1, DenseFloatMatrix1D vec2,
 			String term1, String term2 )
-	{		
-		
+	{
+
 		// Process:
 		// a) get fft 1
 		// b) get fft 2
 		// c) complex element-wise multiply
 		// d) inverse fft
-		
+
 		int dim = vec1.size();
 		DenseFloatMatrix1D result;
 		DenseFComplexMatrix1D C;
 		DenseFComplexMatrix1D D;
-		
+
 		// Check if we are able to use the fft cache
 		if (term1!=null)
-		{			
+		{
 			C = getFFT( term1, vec1 );
-			
+
 		}
-		else C = vec1.getFft(); 
-		
+		else C = vec1.getFft();
+
 		if (term2!=null)
 		{
 			D = getFFT( term2, vec2 );
 		}
-		else D = vec2.getFft(); 	
-				
+		else D = vec2.getFft();
+
 		//**************************************
 		// Use this when not using fft cache
-		//C = vec1.getFft(); 
-		//D = vec2.getFft(); 
+		//C = vec1.getFft();
+		//D = vec2.getFft();
 		//**************************************
-		
+
 		// Do convolution in the fourier domain
-		// First: Element-wise complex multiplication		
+		// First: Element-wise complex multiplication
 		for (int i=0; i<dim; i++ )
-		{			
+		{
 			D.setQuick( i, FComplex.mult( C.getQuick(i), D.getQuick(i) ) );
 		}
-		
+
 		// Second:
 		// Inverse transform
 		D.ifft(true);
-		
+
 		// Count number of convolutions
 		convCount++;
-				
+
 		result = ((DenseFloatMatrix1D)(D.getRealPart()));
 		normalize(result);
-				
-		return result;		
+
+		return result;
 	}
-	
+
 	public DenseFloatMatrix1D doConvolveFFT( DenseFloatMatrix1D vec1, DenseFloatMatrix1D vec2 )
-	{		
+	{
 		// get fft 1
 		// get fft 2
 		// complex element-wise multiply
 		// inverse fft
-		
+
 		int dim = vec1.size();
 		DenseFloatMatrix1D result;
 		DenseFComplexMatrix1D C = vec1.getFft();
-		DenseFComplexMatrix1D D = vec2.getFft();	
-				
+		DenseFComplexMatrix1D D = vec2.getFft();
+
 		for (int i=0; i<dim; i++ )
-		{			
+		{
 			C.setQuick( i, FComplex.mult( C.getQuick(i), D.getQuick(i) ) );
 		}
-		
+
 		C.ifft(true);
-		
+
 		convCount++;
-				
+
 		result = ((DenseFloatMatrix1D)(C.getRealPart()));
 		normalize(result);
-				
-		return result;	
+
+		return result;
 	}
-	
+
 	public int[] makeScrambledIntArray( int size )
 	{
 		int[] array = new int[size];
-						
+
 		for (int i=0; i<size; i++)
 		{
-			array[i] = i;			
+			array[i] = i;
 		}
-				
+
 		FloatRandomEngine rEng = new FRand(new java.util.Date());
-		
+
 		int idx1, idx2, tmp;
-		
+
 		for (int i=0; i<size; i++)
 		{
 			// Do some swapping
 			idx1 = (int)(rEng.nextFloat()*size);
 			idx2 = (int)(rEng.nextFloat()*size);
-			
-			tmp = array[idx1];			
+
+			tmp = array[idx1];
 			array[idx1] = array[idx2];
 			array[idx2] = tmp;
 		}
-		
+
 		return array;
 	}
-	
+
 	public DenseFloatMatrix1D scrambleVector( DenseFloatMatrix1D vec, int[] permVector )
 	{
 		DenseFloatMatrix1D scramVec = new DenseFloatMatrix1D(vec.size());
-		
+
 		if (vec.size()!=permVector.length)
 		{
 			System.out.println("scrambleVector: Size of vectors do not match");
 			return scramVec;
 		}
-		
+
 		for (int i=0; i<vec.size(); i++ )
 		{
 			scramVec.setQuick( permVector[i], vec.getQuick(i) );
 		}
-						
+
 		return scramVec;
 	}
-		
+
 	public DenseFloatMatrix1D rotateVector( DenseFloatMatrix1D vec, int places )
 	{
 		DenseFloatMatrix1D rotVec = new DenseFloatMatrix1D(vec.size());
 		int idx;
-		
+
 		for (int i=0; i<vec.size(); i++ )
 		{
 			idx = i+places;
 			if (idx>=vec.size()) idx = idx - vec.size();
 			if (idx<0) idx = idx + vec.size();
-			
+
 			rotVec.setQuick( idx, vec.getQuick(i) );
-		}						
+		}
 		return rotVec;
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 
 	}
 
--- trunk/src/pitt/search/semanticvectors/DocVectors.java
+++ trunk/src/pitt/search/semanticvectors/DocVectors.java
@@ -1,36 +1,36 @@
 /**
-	 Copyright (c) 2007, University of Pittsburgh
+   Copyright (c) 2007, University of Pittsburgh
 
-	 All rights reserved.
+   All rights reserved.
 
-	 Redistribution and use in source and binary forms, with or without
-	 modification, are permitted provided that the following conditions are
-	 met:
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
 
-	 * Redistributions of source code must retain the above copyright
-	 notice, this list of conditions and the following disclaimer.
+   * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
 
-	 * Redistributions in binary form must reproduce the above
-	 copyright notice, this list of conditions and the following
-	 disclaimer in the documentation and/or other materials provided
-	 with the distribution.
+   * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
 
-	 * Neither the name of the University of Pittsburgh nor the names
-	 of its contributors may be used to endorse or promote products
-	 derived from this software without specific prior written
-	 permission.
+   * Neither the name of the University of Pittsburgh nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
 
-	 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	 LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	 A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-	 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	 EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	 PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	 PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **/
 
 package pitt.search.semanticvectors;
@@ -44,7 +44,7 @@ import java.util.Hashtable;
 import java.util.Random;
 import java.util.Enumeration;
 
-/** 
+/**
  * Implementation of vector store that collects doc vectors by
  * iterating through all the terms in a term vector store and
  * incrementing document vectors for each of the documents containing
@@ -56,112 +56,112 @@ import java.util.Enumeration;
  */
 public class DocVectors implements VectorStore {
 
-	private VectorStoreRAM docVectors;
-	private TermVectorsFromLucene termVectorData;
-	private IndexReader indexReader;
+  private VectorStoreRAM docVectors;
+  private TermVectorsFromLucene termVectorData;
+  private IndexReader indexReader;
 
-	/**
-	 * Constructor that gets everything it needs from a
-	 * TermVectorsFromLucene object.
-	 */
-	public DocVectors (TermVectorsFromLucene termVectorData) throws IOException {
-		this.termVectorData = termVectorData;
-		this.indexReader = termVectorData.getIndexReader();
-		this.docVectors = new VectorStoreRAM();
+  /**
+   * Constructor that gets everything it needs from a
+   * TermVectorsFromLucene object.
+   */
+  public DocVectors (TermVectorsFromLucene termVectorData) throws IOException {
+    this.termVectorData = termVectorData;
+    this.indexReader = termVectorData.getIndexReader();
+    this.docVectors = new VectorStoreRAM();
 
-		// Intialize doc vector store.
-		System.err.println("Initializing document vector store ...");
-		for (int i = 0; i < indexReader.numDocs(); ++i) {
-			float[] docVector = new float[Flags.dimension];
-			for (int j = 0; j < Flags.dimension; ++j) {
-				docVector[j] = 0;
-			}
-			this.docVectors.putVector(Integer.toString(i), docVector);
-		}
+    // Intialize doc vector store.
+    System.err.println("Initializing document vector store ...");
+    for (int i = 0; i < indexReader.numDocs(); ++i) {
+      float[] docVector = new float[Flags.dimension];
+      for (int j = 0; j < Flags.dimension; ++j) {
+        docVector[j] = 0;
+      }
+      this.docVectors.putVector(Integer.toString(i), docVector);
+    }
 
-		// Create doc vectors, iterating over terms.
-		System.out.println("Building document vectors ...");
-		Enumeration<ObjectVector> termEnum = termVectorData.getAllVectors();
+    // Create doc vectors, iterating over terms.
+    System.out.println("Building document vectors ...");
+    Enumeration<ObjectVector> termEnum = termVectorData.getAllVectors();
 
-		try {
-			int dc = 0;
-			while (termEnum.hasMoreElements()) {
-				// Output progress counter.
-				if ((dc % 10000 == 0) || (dc < 10000 && dc % 1000 == 0)) {
-					System.err.print(dc + " ... ");
-				}
-				dc++;
+    try {
+      int dc = 0;
+      while (termEnum.hasMoreElements()) {
+        // Output progress counter.
+        if ((dc % 10000 == 0) || (dc < 10000 && dc % 1000 == 0)) {
+          System.err.print(dc + " ... ");
+        }
+        dc++;
 
-				ObjectVector termVectorObject = termEnum.nextElement();
-				float[] termVector = termVectorObject.getVector();
-				String word = (String) termVectorObject.getObject();
+        ObjectVector termVectorObject = termEnum.nextElement();
+        float[] termVector = termVectorObject.getVector();
+        String word = (String) termVectorObject.getObject();
 
-				// Go through checking terms for each fieldName.
-				for (String fieldName: termVectorData.getFieldsToIndex()) {
-					Term term = new Term(fieldName, word);
-					// Get any docs for this term.
-					TermDocs td = this.indexReader.termDocs(term);
-					while (td.next()) {
-						String docID = Integer.toString(td.doc());
-						// Add vector from this term, taking freq into account.
-						float[] docVector = this.docVectors.getVector(docID);
-						for (int j = 0; j < Flags.dimension; ++j) {
-							docVector[j] += td.freq() * termVector[j];
-						}
-					}
-				}
-			}
-		}
-		catch (IOException e) { // catches from indexReader.
-			e.printStackTrace();
-		}
+        // Go through checking terms for each fieldName.
+        for (String fieldName: termVectorData.getFieldsToIndex()) {
+          Term term = new Term(fieldName, word);
+          // Get any docs for this term.
+          TermDocs td = this.indexReader.termDocs(term);
+          while (td.next()) {
+            String docID = Integer.toString(td.doc());
+            // Add vector from this term, taking freq into account.
+            float[] docVector = this.docVectors.getVector(docID);
+            for (int j = 0; j < Flags.dimension; ++j) {
+              docVector[j] += td.freq() * termVector[j];
+            }
+          }
+        }
+      }
+    }
+    catch (IOException e) { // catches from indexReader.
+      e.printStackTrace();
+    }
 
-		System.err.println("\nNormalizing doc vectors ...");
-		int dc = 0;
-		for (int i = 0; i < indexReader.numDocs(); ++i) {
-			float[] docVector = this.docVectors.getVector(Integer.toString(i));
-			docVector = VectorUtils.getNormalizedVector(docVector);
-			this.docVectors.putVector(Integer.toString(i), docVector);
-		}
-	}
+    System.err.println("\nNormalizing doc vectors ...");
+    int dc = 0;
+    for (int i = 0; i < indexReader.numDocs(); ++i) {
+      float[] docVector = this.docVectors.getVector(Integer.toString(i));
+      docVector = VectorUtils.getNormalizedVector(docVector);
+      this.docVectors.putVector(Integer.toString(i), docVector);
+    }
+  }
 
-	/**
-	 * Create a version of the vector store indexes by path / filename rather than Lucene ID.
-	 */
-	public VectorStore makeWriteableVectorStore() {
-		VectorStoreRAM outputVectors = new VectorStoreRAM();
+  /**
+   * Create a version of the vector store indexes by path / filename rather than Lucene ID.
+   */
+  public VectorStore makeWriteableVectorStore() {
+    VectorStoreRAM outputVectors = new VectorStoreRAM();
 
-		for (int i = 0; i < this.indexReader.numDocs(); ++i) {
-			String docName;
-			try {
-				if (this.indexReader.document(i).getField("path") != null) {
-					docName = this.indexReader.document(i).getField("path").stringValue();
-				} else {
-					// For bilingual docs, we index "filename" not "path",
-					// since there are two system paths, one for each
-					// language. So if there was no "path", get the "filename".
-					docName = this.indexReader.document(i).getField("filename").stringValue();
-				}
-				float[] docVector = this.docVectors.getVector(Integer.toString(i));
-				outputVectors.putVector(docName, docVector);
-			} catch (CorruptIndexException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-		}
-		return outputVectors;
-	}
+    for (int i = 0; i < this.indexReader.numDocs(); ++i) {
+      String docName;
+      try {
+        if (this.indexReader.document(i).getField("path") != null) {
+          docName = this.indexReader.document(i).getField("path").stringValue();
+        } else {
+          // For bilingual docs, we index "filename" not "path",
+          // since there are two system paths, one for each
+          // language. So if there was no "path", get the "filename".
+          docName = this.indexReader.document(i).getField("filename").stringValue();
+        }
+        float[] docVector = this.docVectors.getVector(Integer.toString(i));
+        outputVectors.putVector(docName, docVector);
+      } catch (CorruptIndexException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return outputVectors;
+  }
 
-	public float[] getVector(Object id) {
-		return this.docVectors.getVector(id);
-	}
+  public float[] getVector(Object id) {
+    return this.docVectors.getVector(id);
+  }
 
-	public Enumeration getAllVectors() {
-		return this.docVectors.getAllVectors();
-	}
+  public Enumeration getAllVectors() {
+    return this.docVectors.getAllVectors();
+  }
 
-	public int getNumVectors() {
-		return this.docVectors.getNumVectors();
-	}
-}    
+  public int getNumVectors() {
+    return this.docVectors.getNumVectors();
+  }
+}
