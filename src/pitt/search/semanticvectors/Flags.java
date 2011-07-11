79b6929371b497d5b6b3edc44ee22d257528b0c9
TreverseC <TreverseC@483481f0-a63c-0410-a8a8-396719eec1a4>
2011-07-11 21:52:54 +0000

    Implemented "standard_deviations" flag for searching, which uses a simple and approximate one-pass algorithm to estimate the standard deviations above the mean association strength across all search results for the top results returned (in response to the "thresholding" thread awhile back). This can be used in conjunction with the new "searchresultsminscore" flag.

--- src/pitt/search/semanticvectors/Flags.java
+++ src/pitt/search/semanticvectors/Flags.java
@@ -93,6 +93,10 @@ public class Flags {
   public static final String porterstemmerDescription =
       "Set to true when using IndexFilePositions if you would like to stem terms";
 
+  public static boolean standard_deviations = false;
+  public static final String standard_deviationsDescription = 
+	  "Set to true when you would prefer results scored as SD above the mean across all search vectors";
+  
   public static boolean usetermweightsinsearch = false;
   public static final String usetermweightsinsearchDescription =
       "Set to true only if you want to scale each comparison score by a term weight during search.";
