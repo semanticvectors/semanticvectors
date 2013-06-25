f8c38a624b2df1cf29fbfefb68057501d9806e21
treversec@gmail.com <treversec@gmail.com@483481f0-a63c-0410-a8a8-396719eec1a4>
2013-06-25 17:54:25 +0000

    Removed some vestigial code that made it in on the last commit

--- trunk/src/pitt/search/semanticvectors/vectors/BinaryVector.java
+++ trunk/src/pitt/search/semanticvectors/vectors/BinaryVector.java
@@ -14,7 +14,6 @@ import org.apache.lucene.util.OpenBitSet;
 import pitt.search.semanticvectors.FlagConfig;
 import pitt.search.semanticvectors.VectorStore;
 import pitt.search.semanticvectors.hashing.Bobcat;
-import pitt.search.semanticvectors.orthography.NumberRepresentation;
 
 /**
  * Binary implementation of Vector.
@@ -43,8 +42,6 @@ public class BinaryVector implements Vector {
   
   private static final int DEBUG_PRINT_LENGTH = 64;
   private Random random;
-  private static NumberRepresentation numberRepresentation;
-  private static VectorStore numberVectors;
   private final int dimension;
   
   /**
@@ -83,15 +80,7 @@ public class BinaryVector implements Vector {
     this.bitSet = new OpenBitSet(dimension);
     this.isSparse = true;
     this.random = new Random();
-    
-    if (false) //(BinaryVector.numberVectors == null)
-    {	
-    	int d2 = dimension/2;
-    	String[] fakeArgs = {"-dimension",dimension+"","-vectortype","binary","-seedlength",""+d2};
-    	BinaryVector.numberRepresentation = new NumberRepresentation(FlagConfig.getFlagConfig(fakeArgs));
-    	BinaryVector.numberVectors = BinaryVector.numberRepresentation.getNumberVectors(1, 100);
-    }
-    
+     
     }
 
   /**
