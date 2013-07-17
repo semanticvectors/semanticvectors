82c42b1767d0aa0f21d9aabeb4f8e32817d6182d
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2013-07-17 01:13:09 +0000

    Added / changed some error messages while using PSI.

--- src/pitt/search/lucene/LuceneIndexFromTriples.java
+++ src/pitt/search/lucene/LuceneIndexFromTriples.java
@@ -8,6 +8,9 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
+
+import pitt.search.semanticvectors.VerbatimLogger;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -17,19 +20,15 @@ import java.io.IOException;
  * This class takes as input a single text file with each line following the format 
  * <subject>\t<predicate>\t<object> and produces a Lucene index, in which each 
  * "document" is a single tab-delimited predication (or triple) with the fields subject, 
- * predicate, and object
- * 
+ * predicate, and object.
  */
 public class LuceneIndexFromTriples {
 
   private LuceneIndexFromTriples() {}
   static final File INDEX_DIR = new File("predication_index");
 
-
-
   /** Index all text files under a directory. */
   public static void main(String[] args) {
-
     String usage = "java pitt.search.lucene.LuceneIndexFromTriples [triples text file] ";
     if (args.length == 0) {
       System.err.println("Usage: " + usage);
@@ -48,15 +47,15 @@ public class LuceneIndexFromTriples {
           LUCENE_VERSION, new WhitespaceAnalyzer(LUCENE_VERSION));
       IndexWriter writer = new IndexWriter(FSDirectory.open(INDEX_DIR), writerConfig);
 
-      final File docDir = new File(args[0]);
-      if (!docDir.exists() || !docDir.canRead()) {
+      final File triplesTextFile = new File(args[0]);
+      if (!triplesTextFile.exists() || !triplesTextFile.canRead()) {
         writer.close();
-        throw new IOException("Document file '" + docDir.getAbsolutePath() +
+        throw new IOException("Document file '" + triplesTextFile.getAbsolutePath() +
             "' does not exist or is not readable, please check the path");
       }
 
       System.out.println("Indexing to directory '" +INDEX_DIR+ "'...");
-      indexDoc(writer, docDir);
+      indexDoc(writer, triplesTextFile);
       writer.close();       
     } catch (IOException e) {
       System.out.println(" caught a " + e.getClass() +
@@ -64,27 +63,26 @@ public class LuceneIndexFromTriples {
     }
   }
 
-
   /**
    * This class indexes the file passed as a parameter, writing to the index passed as a parameter.
    * Each predication is indexed as an individual document, with the fields "subject", "predicate", and "object"
 
    * @throws IOException
    */
-  static void indexDoc(IndexWriter fsWriter, File file) throws IOException {
-    BufferedReader theReader = new BufferedReader(new FileReader(file));
+  static void indexDoc(IndexWriter fsWriter, File triplesTextFile) throws IOException {
+    BufferedReader theReader = new BufferedReader(new FileReader(triplesTextFile));
     String lineIn = theReader.readLine();
-    lineIn = theReader.readLine();
     int linecnt = 0;
-    while (lineIn != null)  {   
+    while ((lineIn = theReader.readLine()) != null)  {   
       java.util.StringTokenizer theTokenizer = new java.util.StringTokenizer(lineIn,"\t");
       // Output progress counter.
       if( ( ++linecnt % 10000 == 0 ) || ( linecnt < 10000 && linecnt % 1000 == 0 ) ){
-        System.err.print((linecnt) + " ... ");
+        VerbatimLogger.info((linecnt) + " ... ");
       }
       try {
         if (theTokenizer.countTokens() < 3) {
-          lineIn = theReader.readLine();
+          VerbatimLogger.warning(
+              "Line in predication file does not have three delimited fields: " + lineIn + "\n");
           continue;
         }
 
@@ -99,14 +97,12 @@ public class LuceneIndexFromTriples {
         doc.add(new TextField("predication",subject+predicate+object, Field.Store.NO));
         fsWriter.addDocument(doc);
       }
-      catch (Exception e)
-      {
+      catch (Exception e) {
         System.out.println(lineIn);
         e.printStackTrace();
       }
-
-      lineIn = theReader.readLine();
     }
+    VerbatimLogger.info("\n");  // Newline after line counter prints.
     theReader.close();
   }
 }
