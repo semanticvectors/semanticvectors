b7113754cc446eafbcec12559eb1ab1285f7dab7
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2013-11-11 04:53:42 +0000

    Setting up project for Maven.
    This includes moving source directories to the src/main/java and src/test/java patterns.

---
+++ src/test/java/pitt/search/semanticvectors/integrationtests/RegressionTests.java
@@ -0,0 +1,285 @@
+/**
+   Copyright 2009, The SemanticVectors AUTHORS.
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following disclaimer
+   in the documentation and/or other materials provided with the
+   distribution.
+
+ * Neither the name of Google Inc. nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+package pitt.search.semanticvectors.integrationtests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import org.junit.*;
+
+import pitt.search.semanticvectors.BuildIndex;
+import pitt.search.semanticvectors.BuildPositionalIndex;
+import pitt.search.semanticvectors.FlagConfig;
+import pitt.search.semanticvectors.Search;
+import pitt.search.semanticvectors.SearchResult;
+import pitt.search.semanticvectors.integrationtests.RunTests;
+import static org.junit.Assert.*;
+
+/**
+ * A collection of regression tests, to make sure that new options and
+ * features don't break existing usage patterns.
+ *
+ * Should be run using "ant run-integration-tests", which will run
+ * regression using the RunTests class working in the
+ * test/testdata/tmp directory. Depends on there being appropriate
+ * Lucene indexes in this directory, which are prepared by the
+ * RunTests class.
+ */
+public class RegressionTests {
+  @Before
+  public void setUp() throws IOException {
+    RunTests.prepareTestData();
+  }
+
+  private int buildSearchGetRank(String buildCmd, String searchCmd, String targetResult) {
+    String[] filesToBuild = new String[] {"termvectors.bin", "docvectors.bin"};
+    String[] buildArgs = buildCmd.split("\\s+");
+    String[] searchArgs = searchCmd.split("\\s+");
+    for (String fn : filesToBuild) {
+      File file = new File(fn);
+      if (file.isFile()) {
+        assertTrue("Failed to delete file: " + fn, file.delete());
+      }
+      file = null;
+      assertFalse("File appears to be still present: " + fn, (new File(fn)).isFile());
+    }
+    try {
+      BuildIndex.main(buildArgs);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    for (String fn: filesToBuild) assertTrue((new File(fn)).isFile());
+
+    List<SearchResult> results = Search.RunSearch(FlagConfig.getFlagConfig(searchArgs));
+    int rank = 1;
+    boolean found = false;
+    if (results.isEmpty()) {
+      throw new RuntimeException("Results were empty!");
+    } else {
+      for (SearchResult result : results) {
+        String term = (String) result.getObjectVector().getObject();
+        if (term.equals(targetResult)) {
+          found = true;
+          break;
+        }
+        ++rank;
+      }
+    }
+    if (!found) {
+      fail("Failed to get matching results with search terms: " + searchCmd);
+    }
+    
+    for (String fn: filesToBuild) assertTrue((new File(fn)).delete());
+    return rank;
+  }
+
+  @Test
+  public void testBuildAndSearchBasicRealIndex() {
+    assertEquals(2, buildSearchGetRank("-dimension 200 -luceneindexpath positional_index",
+        "-queryvectorfile termvectors.bin -searchvectorfile termvectors.bin peter", "simon"));
+  }
+   
+  @Test
+  public void testBuildAndSearchBasicRealIndexDocs() {
+    assertTrue(3 > buildSearchGetRank("-dimension 200 -luceneindexpath positional_index",
+        "-queryvectorfile termvectors.bin -searchvectorfile docvectors.bin peter",
+        "src/test/resources/testdata/John/Chapter_21"));
+  }
+
+  @Test
+  public void testBuildAndSearchBasicComplexIndex() {
+    assertEquals(2, buildSearchGetRank(
+        "-dimension 200 -vectortype complex -luceneindexpath positional_index", "peter", "simon"));
+  }
+
+  @Test
+  public void testBuildAndSearchBasicBinaryIndex() {
+    assertEquals(2, buildSearchGetRank(
+        "-dimension 8192 -seedlength 128 -vectortype binary -luceneindexpath positional_index", "peter", "simon"));
+  }
+
+  private int positionalBuildSearchGetRank(
+      String buildCmd, String searchCmd, String[] filesToBuild, String targetResult) {
+    String[] buildArgs = buildCmd.split("\\s+");
+    String[] searchArgs = searchCmd.split("\\s+");
+
+    for (String fn : filesToBuild) {
+      File file = new File(fn);
+      if (file.isFile()) {
+        assertTrue("Failed to delete file: " + fn, file.delete());
+      }
+      file = null;
+      assertFalse("File appears to be still present: " + fn, (new File(fn)).isFile());
+    }
+    BuildPositionalIndex.main(buildArgs);
+    for (String fn : filesToBuild) {
+      assertTrue("Missing required file: " + fn, new File(fn).isFile());
+    }
+
+    List<SearchResult> results = Search.RunSearch(FlagConfig.getFlagConfig(searchArgs));
+    assertFalse("Search results should not be empty!", results.isEmpty());
+
+    int rank = 1;
+    for (SearchResult result : results) {
+      String term = (String) result.getObjectVector().getObject();
+      if (term.equals(targetResult)) break;
+      ++rank;
+    }
+
+    for (String fn : filesToBuild) assertTrue(new File(fn).delete());
+    return rank;
+  }
+
+  @Test
+  public void testBuildAndSearchRealPositionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype real -seedlength 10 -luceneindexpath positional_index",
+        "-queryvectorfile termtermvectors.bin simon",
+        new String[] {"termtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank < 5);
+  }
+    
+  @Test
+  synchronized public void testBuildAndSearchRealPositionalIndexDocs() {
+    int chapter17Rank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype real -seedlength 10 -luceneindexpath positional_index",
+        "-queryvectorfile termtermvectors.bin -searchvectorfile docvectors.bin bread",
+        new String[] {"termtermvectors.bin", "docvectors.bin"},
+        "src/test/resources/testdata/John/Chapter_17");
+    assertTrue(chapter17Rank < 5);
+  }
+  
+  @Test
+  public void testBuildAndSearchComplexPositionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype complex -seedlength 10 -luceneindexpath positional_index",
+        "-queryvectorfile termtermvectors.bin simon",
+        new String[] {"termtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank < 5);
+  }
+  
+  @Test
+  public void testBuildAndSearchBinaryPositionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 8192 -vectortype binary -seedlength 4096 -luceneindexpath positional_index",
+        "-queryvectorfile termtermvectors.bin simon",
+        new String[] {"termtermvectors.bin", "docvectors.bin"},
+        "peter");
+       assertTrue(peterRank < 5);
+  }
+
+  // Convolution for complex directional indexing seems to really need some termweighting to work well. 
+  @Test
+  public void testBuildAndSearchComplexDirectionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype complex -seedlength 10 -positionalmethod directional -termweight idf "
+        + "-luceneindexpath positional_index",
+        "-queryvectorfile drxntermvectors.bin simon",
+        new String[] {"drxntermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank <= 3);
+  }
+  
+  @Test
+  public void testBuildAndSearchRealDirectionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype real -seedlength 10 -positionalmethod directional -luceneindexpath positional_index",
+        "-queryvectorfile drxntermvectors.bin simon",
+        new String[] {"drxntermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank <= 3);
+  }
+
+  @Test
+  public void testBuildAndSearchBinaryDirectionalIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 2048 -vectortype binary -seedlength 1024 -positionalmethod directional "
+        + "-termweight none -luceneindexpath positional_index",
+        "-queryvectorfile drxntermvectors.bin simon",
+        new String[] {"drxntermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank <= 3);
+  }
+
+  @Test
+  public void testBuildAndSearchRealPermutationIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype real -seedlength 10 -positionalmethod permutation -luceneindexpath positional_index",
+        "-searchtype permutation -queryvectorfile elementalvectors.bin -searchvectorfile permtermvectors.bin simon ?",
+        new String[] {"elementalvectors.bin", "permtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank <= 2);
+  }
+
+  @Test
+  public void testBuildAndSearchComplexPermutationIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype complex -seedlength 10 -positionalmethod permutation -luceneindexpath positional_index",
+        "-searchtype permutation -queryvectorfile elementalvectors.bin -searchvectorfile permtermvectors.bin simon ?",
+        new String[] {"elementalvectors.bin", "permtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank < 3);
+  }
+
+  @Test
+  public void testBuildAndSearchBinaryPermutationIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 1024 -vectortype binary -seedlength 512 -positionalmethod permutation -luceneindexpath positional_index",
+        "-searchtype permutation -queryvectorfile elementalvectors.bin -searchvectorfile permtermvectors.bin simon ?",
+        new String[] {"elementalvectors.bin", "permtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(3 >= peterRank);
+  }
+
+  /*
+   * This last test seems to throw lots of others off in Windows. I wonder if there's
+   * some multithreading going on that makes this whole test suite very unsafe - not sure.
+   * Test still causes problems with regular permutation search, so I don't think it's a problem
+   * with balanced_permutation itself.
+   */
+  /*
+  @Test
+  public void testBuildAndSearchRealBalancedPermutationIndex() {
+    int peterRank = positionalBuildSearchGetRank(
+        "-dimension 200 -vectortype real -seedlength 10 -positionalmethod permutation positional_index",
+        "-queryvectorfile elementalvectors.bin -searchvectorfile permtermvectors.bin -searchtype balanced_permutation simon ?",
+        new String[] {"elementalvectors.bin", "permtermvectors.bin", "docvectors.bin"},
+        "peter");
+    assertTrue(peterRank < 5);
+  }
+  */
+}
---
+++ src/test/java/pitt/search/semanticvectors/vectors/ComplexVectorTest.java
@@ -0,0 +1,242 @@
+/**
+   Copyright (c) 2011, the SemanticVectors AUTHORS.
+
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+
+   * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
+
+   * Neither the name of the University of Pittsburgh nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+package pitt.search.semanticvectors.vectors;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.IndexOutput;
+import org.apache.lucene.store.RAMDirectory;
+import org.junit.Test;
+
+import static pitt.search.semanticvectors.MyTestUtils.assertFloatArrayEquals;
+import static pitt.search.semanticvectors.vectors.ComplexVector.Mode;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Tests for {@code ComplexVector} class.  Many of these tests use {@code ComplexVector#toString}
+ * to check correct results.  This is somewhat ad hoc and any change in the debug string
+ * representation will likely cause breakages.  Be warned!
+ */
+public class ComplexVectorTest extends TestCase {
+
+  private static final double TOL = 0.001;
+
+  @Test 
+  public void testGenerateRandomVector() {
+    // Generate random vector
+    ComplexVector cv1 = (ComplexVector) VectorFactory.generateRandomVector(
+        VectorType.COMPLEX, 5, 2, new Random(0));
+    assertArrayEquals(new short[] {0, 13622, 4, 9934}, cv1.getSparseOffsets());
+    assertEquals(Mode.POLAR_SPARSE, cv1.getOpMode());
+  }
+  
+  @Test
+  public void testDenseToCartesian() {
+    ComplexVector vector = new ComplexVector(new short[] {-1, 0});
+    vector.toCartesian();
+    assertEquals(ComplexVector.Mode.CARTESIAN, vector.getOpMode());
+    assertFloatArrayEquals(new float[] {0.0f, 0.0f, 1.0f, 0.0f}, vector.getCoordinates(), TOL);
+  }
+
+  @Test
+  public void testComplexVectorConversion() {
+    ComplexVector vector = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, 10);
+    assertTrue(vector.isZeroVector());
+    assertEquals(10, vector.getDimension());
+
+    vector = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, 2);
+    vector.setSparseOffsets(new short[] {1, 0});
+    vector.toDensePolar();
+    assertArrayEquals(new short[] {-1, 0}, vector.getPhaseAngles());
+    vector.toCartesian();
+    assertFloatArrayEquals(new float[] {0, 0, 1, 0}, vector.getCoordinates(), TOL);
+  }
+
+  @Test
+  public void testWriteToString() {
+    float[] coords = { 12.3f, 3.2f, 2.6f, -1.3f, -0.01f, -1000.2f };
+    ComplexVector cv = new ComplexVector(coords);
+    assertTrue(cv.writeToString().contains("12.3|3.2|2.6|-1.3|-0.01|-1000.2"));
+    assertTrue(cv.toString().contains("12.3 3.2 2.6 -1.3 -0.01 -1000.2 "));
+  }
+
+  @Test
+  public void testSuperposeSparseOnZero() {
+    int dim = 4;
+    ComplexVector cv1 = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, dim);
+    cv1.setSparseOffsets(new short[] {1, 0, 3, CircleLookupTable.PHASE_RESOLUTION / 4});
+    ComplexVector cv2 = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, dim);
+    cv2.superpose(cv1, 5, null);
+    assertEquals(Mode.CARTESIAN, cv2.getOpMode());
+    assertFloatArrayEquals(
+        new float[] {0, 0, 5, 0, 0, 0, 0, 5}, cv2.getCoordinates(), TOL);
+  }
+
+  @Test
+  public void testSuperposeZeroOnSparse() {
+    int dim = 4;
+    int seedLength = 2;
+    ComplexVector cv1 = (ComplexVector) VectorFactory.generateRandomVector(
+        VectorType.COMPLEX, dim, seedLength, new Random(0));
+    assertArrayEquals(new short[] {2, 13622, 0, 9934}, cv1.getSparseOffsets());
+    ComplexVector cv2 = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, dim);
+    cv2.superpose(cv1, 1.0, null);
+    assertFloatArrayEquals(
+        new float[] {-0.78503f, -0.61945f, 0, 0, 0.48955f, -0.872064f, 0, 0},
+        cv2.getCoordinates(), TOL);
+  }
+
+  @Test
+  public void testVectorCopyCartesian() {
+    float[] coords = { 12.3f, 3.2f, 2.6f, -1.3f };
+    ComplexVector cv1 = new ComplexVector(coords);
+    ComplexVector cv2 = cv1.copy();
+    assertFloatArrayEquals(
+        new float[] { 12.3f, 3.2f, 2.6f, -1.3f }, cv2.getCoordinates(), TOL);
+  }
+
+  @Test
+  public void testCartesianToDensePolar() {
+    ComplexVector cv = new ComplexVector( new float[] {1.0f, 1.0f, 0, 1.0f, 1.0f, 0 } );
+    cv.toDensePolar();
+    assertArrayEquals(new short[] {2048, 4096, 0}, cv.getPhaseAngles());
+  }
+
+  @Test
+  public void testNormalize() {
+    ComplexVector.setDominantMode(Mode.CARTESIAN);
+    ComplexVector cv = new ComplexVector(new float[] {3, 4, 4, 3, 4, 3, 0, 5});
+    cv.normalize();
+    System.out.println(cv.toString());
+    // Note that normalize() makes each individual pair give a coordinate on the unit circle.
+    assertFloatArrayEquals(new float[] {0.6f, 0.8f, 0.8f, 0.6f, 0.8f, 0.6f, 0.0f, 1.0f},
+        cv.getCoordinates(), TOL);
+    ComplexVector.setDominantMode(Mode.POLAR_DENSE);
+    cv.normalize();
+    assertArrayEquals(new short[] {2417, 1676, 1676, 4096}, cv.getPhaseAngles());
+  }
+
+  @Test
+  public void testMeasurePolarOverlap() {
+    int RES = CircleLookupTable.PHASE_RESOLUTION;
+    short[] angles1 = { 0, 0, 0 };
+    short[] angles2 = { 0, -1, (short) (RES / 4) };  // Remember that -1 angle means complex zero.
+    short[] angles3 = { (short) (3*RES / 4), (short) (RES / 2), 0 };
+
+    ComplexVector cv1 = new ComplexVector(angles1);
+    ComplexVector cv2 = new ComplexVector(angles2);
+    ComplexVector cv3 = new ComplexVector(angles3);
+
+    assertEquals(1/3f, cv1.measurePolarDenseOverlap(cv2), TOL);
+    assertEquals(0, cv1.measurePolarDenseOverlap(cv3), TOL);
+    assertEquals(0, cv2.measurePolarDenseOverlap(cv3), TOL);
+    assertEquals(1, cv1.measurePolarDenseOverlap(cv1), TOL);
+    assertEquals(1, cv2.measurePolarDenseOverlap(cv2), TOL);  // Zero entry doesn't contribute.
+    assertEquals(1, cv3.measurePolarDenseOverlap(cv3), TOL);
+  }
+  
+  @Test
+  public void testConvolve() {
+    short RES = CircleLookupTable.PHASE_RESOLUTION;
+    short ZERO_INDEX = CircleLookupTable.ZERO_INDEX;
+    ComplexVector cv = new ComplexVector(new short[] {ZERO_INDEX, 0, (short) (RES/2)});
+    ComplexVector cv2 = new ComplexVector(new short[] {0, (short) (RES/4), (short) (RES/4)});
+    cv.convolve(cv2, 1);
+    for (short i : cv.getPhaseAngles()) {
+      System.err.print(i +  " ");
+    }
+    assertArrayEquals(new short[] {0, (short) (RES/4), (short) (3*RES/4)}, cv.getPhaseAngles());
+    // Should have what we started with.
+    cv.convolve(cv2, -1);
+    assertArrayEquals(new short[] {0, 0, (short) (RES/2)}, cv.getPhaseAngles());
+    // Convolving with inverse of itself gives all ones (or zeros).
+    cv.convolve(cv, -1);
+    assertArrayEquals(new short[] {0, 0, 0}, cv.getPhaseAngles());
+  }
+  
+  @Test
+  public void testBindFromRandom() {
+    short ZERO_INDEX = CircleLookupTable.ZERO_INDEX;
+    Random random = new Random(0);
+    ComplexVector cv1 = (ComplexVector) VectorFactory.generateRandomVector(
+        VectorType.COMPLEX, 5, 2, random);
+    ComplexVector cv2 = (ComplexVector) VectorFactory.generateRandomVector(
+        VectorType.COMPLEX, 5, 2, random);
+    cv1.bind(cv2);
+    assertArrayEquals(new short[] {2301, 1917, ZERO_INDEX, ZERO_INDEX, 9934}, cv1.getPhaseAngles());
+  }
+  
+  @Test
+  public void testBindFromZero() {
+    short ZERO_INDEX = CircleLookupTable.ZERO_INDEX;
+    Random random = new Random(0);
+    ComplexVector cv1 = (ComplexVector) VectorFactory.createZeroVector(VectorType.COMPLEX, 5);
+    cv1.toDensePolar();
+    ComplexVector cv2 = (ComplexVector) VectorFactory.generateRandomVector(
+        VectorType.COMPLEX, 5, 2, random);
+    cv1.bind(cv2);
+    assertArrayEquals(
+        new short[] {13622, ZERO_INDEX, ZERO_INDEX, ZERO_INDEX, 9934}, cv1.getPhaseAngles());
+  }
+
+  @Test
+  public void testReadWrite() {
+    Vector v1 = new ComplexVector(new short[] { -1, 8000, 16000 });
+    RAMDirectory directory = new RAMDirectory();
+    try {
+      IndexOutput indexOutput = directory.createOutput("complexvectors.bin", IOContext.DEFAULT);
+      v1.writeToLuceneStream(indexOutput);
+      indexOutput.flush();
+
+      IndexInput indexInput = directory.openInput("complexvectors.bin", IOContext.DEFAULT);
+      ComplexVector cv2 = new ComplexVector(3, Mode.POLAR_SPARSE);
+      cv2.readFromLuceneStream(indexInput);
+      assertFloatArrayEquals(
+          new float[] {0, 0, -0.997290f, 0.073564f, 0.989176f, -0.1467304f},
+          cv2.getCoordinates(), TOL);
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail();
+    }
+    directory.close();
+  }
+}
