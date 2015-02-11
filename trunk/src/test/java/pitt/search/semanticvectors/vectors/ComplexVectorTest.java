2cdcb0170f941e2de051fcc00ebb11eb33378efa
TreverseC@gmail.com <TreverseC@gmail.com@483481f0-a63c-0410-a8a8-396719eec1a4>
2015-02-11 23:45:00 +0000

    (1) Checked in the Hermitian edition of ComplexVector - currently this is switched on by changing a boolean variable at the very end of the code, but as discussed a better thought-out (or at least somewhat thought-out) way of integrating the current cornucopia of complex vector capabilities is in our sights. (2) Restored the "renderPairwiseOrthogonal" code in ComplexVectorUtils, for posterity but mostly for the purpose of passing one of the unit tests. (3) Altered said unit test so it uses this type of orthogonalization.

--- trunk/src/test/java/pitt/search/semanticvectors/vectors/ComplexVectorTest.java
+++ trunk/src/test/java/pitt/search/semanticvectors/vectors/ComplexVectorTest.java
@@ -224,7 +224,8 @@ public class ComplexVectorTest extends TestCase {
     short ZERO_INDEX = CircleLookupTable.ZERO_INDEX;
     ComplexVector cv = new ComplexVector(new short[] {ZERO_INDEX, 0});
     ComplexVector cv2 = new ComplexVector(new short[] {0, 0});
-    VectorUtils.orthogonalizeVectors(Arrays.asList(new Vector[] {cv, cv2}));
+    ComplexVectorUtils.renderPairwiseOrthogonal(cv2, cv);
+    cv2.normalize();
     assertArrayEquals(new short[] {0, ZERO_INDEX}, cv2.getPhaseAngles());
   }
 
