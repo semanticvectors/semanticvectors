596f69c13ce63a42a6cc4ec5c9aaa156f7ffa4cc
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2015-03-09 21:53:21 +0000

    Updates to update-release-docs

--- trunk/update-release-docs.sh
+++ trunk/update-release-docs.sh
@@ -11,13 +11,13 @@
 # ./update-release-docs.sh.
 
 
-VERSION=5.6  # Should be kept in sync with pom.xml
+VERSION=5.8  # Should be kept in sync with pom.xml
 DEST=../javadoc/latest-stable
 mvn javadoc:jar
-#svn rm --force "$DEST"
-#svn ci "$DEST" -m "Removing old stuff for the new release javadocs"
-#mkdir "$DEST"
-#svn add "$DEST"
+svn rm --force "$DEST"
+svn ci "$DEST" -m "Removing old stuff for the new release javadocs"
+mkdir "$DEST"
+svn add "$DEST"
 cd "$DEST"
 jar xvf ../../trunk/target/semanticvectors-$VERSION-javadoc.jar
 svn add --force .
