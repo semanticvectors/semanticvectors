e21129d57a746b06dcd54e5f0cfccdb3af245665
dwiddows <dwiddows@483481f0-a63c-0410-a8a8-396719eec1a4>
2015-03-10 18:05:17 +0000

    Jar checking for update-release-doc script.

--- update-release-docs.sh
+++ update-release-docs.sh
@@ -11,6 +11,8 @@
 # ./update-release-docs.sh.
 
 
+command -v jar >/dev/null 2>&1 || { echo >&2 "Script requires jar but it's not available. Aborting."; exit 1; }
+
 VERSION=5.8  # Should be kept in sync with pom.xml
 DEST=../javadoc/latest-stable
 mvn javadoc:jar
@@ -20,6 +22,7 @@ mkdir "$DEST"
 svn add "$DEST"
 cd "$DEST"
 jar xvf ../../trunk/target/semanticvectors-$VERSION-javadoc.jar
+
 svn add --force .
 find . -name "*.html" | xargs -n 1 svn propset svn:mime-type text/html
 find . -name "*.css" | xargs -n 1 svn propset svn:mime-type text/css
