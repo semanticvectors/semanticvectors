b4ed276571424d222e6b6b55844ecda0d4ca70f6
neophytescientist <neophytescientist@users.noreply.github.com>
2017-03-27 18:02:21 -0500

    Adjusted and tested to make sure it actually builds successfully this time.

--- src/main/java/pitt/search/semanticvectors/ESP.java
+++ src/main/java/pitt/search/semanticvectors/ESP.java
@@ -530,9 +530,9 @@ private void processPredicationDocument(Document document, BLAS blas)
 
         //if timerange, skip encoding predications outside of scope
         if (!flagConfig.timerange().isEmpty()) {
-          short yearstart = Short.parseShort(flagConfig.timerange().split(",")[0]);
-          short yearstop = Short.parseShort(flagConfig.timerange().split(",")[1]);
-          if(!(yearstart <= this.pubyear && this.pubyear <= yearstop)) {
+          Short yearstart = Short.parseShort(flagConfig.timerange().split(",")[0]);
+          Short yearstop = Short.parseShort(flagConfig.timerange().split(",")[1]);
+          if(!(yearstart <= pubyear && pubyear <= yearstop)) {
             logger.fine("Skipping predication: " + subject + " " + predicate + " " + object + ".\t| Not in date range.\n");
             encode = false;
           }
