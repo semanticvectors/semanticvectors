006befac07a3704b9866d51ff4d4fc159acd79ae
tcohen1 <tcohen1@192.168.0.7>
2018-08-29 15:55:48 -0700

    Added option to output to word2vec binary format.

--- src/main/java/pitt/search/semanticvectors/VectorStoreTranslater.java
+++ src/main/java/pitt/search/semanticvectors/VectorStoreTranslater.java
@@ -1,5 +1,5 @@
 /**
-   Copyright 2008, Google Inc.
+  Copyright 2008, Google Inc.
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
@@ -33,10 +33,35 @@
 
 package pitt.search.semanticvectors;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.IllegalArgumentException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Vector;
 
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexOutput;
+
+import pitt.search.semanticvectors.utils.Bobcat;
 import pitt.search.semanticvectors.utils.VerbatimLogger;
+import pitt.search.semanticvectors.vectors.RealVector;
+import pitt.search.semanticvectors.vectors.VectorFactory;
+import pitt.search.semanticvectors.vectors.VectorType;
+import pitt.search.semanticvectors.vectors.ZeroVectorException;
 
 /**
  * Class providing command-line interface for transforming vector
@@ -47,7 +72,7 @@ public class VectorStoreTranslater {
       + "\nUsage: java pitt.search.semanticvector.VectorStoreTranslater -option INFILE OUTFILE"
       + "\n -option can be: -lucenetotext or -texttolucene";
 
-  private enum Options { LUCENE_TO_TEXT, TEXT_TO_LUCENE }
+  private enum Options { LUCENE_TO_TEXT, TEXT_TO_LUCENE, LUCENE_TO_WORD2VEC}
 
   /**
    * Command line method for performing index translation.
@@ -55,15 +80,20 @@ public class VectorStoreTranslater {
    * @see #usageMessage
    */
   public static void main(String[] args) throws IOException {
+	  
+	 final long ONE_GB = (long) Math.pow(1024,3);
+	  
     // Parse command line args.
-    if (args.length != 3) {
-      VerbatimLogger.info("You gave " + args.length + " arguments, there must be exactly 3.\n");
+    if (args.length < 3) {
+      VerbatimLogger.info("You gave " + args.length + " arguments aside from the command line flags, there must be at least 3.\n");
       System.err.println(usageMessage);
       throw new IllegalArgumentException();
     }
     Options option = null;
     if (args[0].equalsIgnoreCase("-lucenetotext")) { option = Options.LUCENE_TO_TEXT; }
     else if (args[0].equalsIgnoreCase("-texttolucene")) { option = Options.TEXT_TO_LUCENE; }
+    else if (args[0].equalsIgnoreCase("-lucenetoword2vec")) { option = Options.LUCENE_TO_WORD2VEC; }
+   
     else {
       System.err.println(usageMessage);
       throw new IllegalArgumentException();
@@ -74,7 +104,7 @@ public class VectorStoreTranslater {
 
     // Empty flag config is needed to satisfy vector store interfaces.
     FlagConfig flagConfig = FlagConfig.getFlagConfig(null);
-    
+
     // Convert Lucene-style index to plain text.
     if (option == Options.LUCENE_TO_TEXT) {
       VectorStoreReaderLucene vecReader;
@@ -86,10 +116,50 @@ public class VectorStoreTranslater {
       VerbatimLogger.info("Writing term vectors to " + outfile + "\n");
       VectorStoreWriter.writeVectorsInTextFormat(outfile, flagConfig, vecReader);
       vecReader.close();
-    }
+    } 
+    else if (option == Options.LUCENE_TO_WORD2VEC) 
+    {
+    	 VectorStoreReaderLucene vecReader;
+         try {
+           vecReader = new VectorStoreReaderLucene(infile, flagConfig);
+         } catch (IOException e) {
+           throw e;
+         }
+         VerbatimLogger.info("Writing term vectors to " + outfile + "\n");
+         OutputStream theOutput = new FileOutputStream(new File(outfile));
+    		
+    	 //java code to output word2vec format, procedure derived from code in repository
+    	 //https://github.com/medallia/Word2VecJava.git
+    	 Charset cs = Charset.forName("UTF-8");
+    	 String header = String.format("%d %d\n", vecReader.getNumVectors(), flagConfig.dimension());
+    	 ByteBuffer buffer = ByteBuffer.allocate(4 * flagConfig.dimension());
+    	 buffer.order(ByteOrder.LITTLE_ENDIAN);	
 
-    // Convert plain text index to Lucene-style.
-    if (option == Options.TEXT_TO_LUCENE) {
+    	 theOutput.write(header.getBytes(cs));
+    	 Enumeration<ObjectVector> allVecs = vecReader.getAllVectors();
+    	
+    		while (allVecs.hasMoreElements())
+    		{
+    			ObjectVector nextObjectVector = allVecs.nextElement();
+    			String nextTerm = nextObjectVector.getObject().toString();
+    			float[] nextVec = ((RealVector) nextObjectVector.getVector()).getCoordinates();
+    			theOutput.write(String.format("%s ", nextTerm).getBytes(cs));
+    			buffer.clear();
+    			
+    			for(int j = 0; j < flagConfig.dimension(); ++j)
+    				buffer.putFloat(nextVec[j]);
+    			
+    			theOutput.write(buffer.array());
+    			theOutput.write('\n');
+    			
+    		}
+    		
+    		theOutput.flush();
+    		theOutput.close();
+          
+    }
+     // Convert plain text index to Lucene-style.
+    else if (option == Options.TEXT_TO_LUCENE) {
       VectorStoreReaderText vecReader;
       try {
         vecReader = new VectorStoreReaderText(infile, flagConfig);
