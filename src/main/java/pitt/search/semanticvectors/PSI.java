9668facb20f35471e17681d9629bdd9317aa734e
Sava <sava.savov@ontotext.com>
2019-12-06 14:18:50 +0200

    GDB-3030 Changed default value of max generated tasks to 20, because of better performance

--- src/main/java/pitt/search/semanticvectors/PSI.java
+++ src/main/java/pitt/search/semanticvectors/PSI.java
@@ -88,7 +88,7 @@ public class PSI {
 	private volatile boolean interrupted = false;
 	private AtomicBoolean isCreationInterruptedByUser;
 
-	private final int BLOCKING_QUEUE_SIZE = Integer.parseInt(System.getProperty("graphdb.predication.max.generated.tasks", "5"));
+	private final int BLOCKING_QUEUE_SIZE = Integer.parseInt(System.getProperty("graphdb.predication.max.generated.tasks", "20"));
 
 	public PSI(FlagConfig flagConfig) {
 		this(flagConfig, new AtomicBoolean(false));
